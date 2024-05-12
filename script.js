ab522a1f2616ac6a5640b2c2230d3a49cc3a81ef
fina <finarifana22@upi.edu>
2024-05-12 18:44:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,25 +12,6 @@ function calculateArithmetic() {
   document.getElementById('result').innerText = "Jumlah deret aritmatika: " + result;
 }
 
-function calculateGeometric() {
-  var a = parseFloat(document.getElementById('input').value);
-  var r = parseFloat(document.getElementById('input2').value);
-  var n = parseInt(document.getElementById('n').value);
-
-  if (isNaN(a) || isNaN(r) || isNaN(n)) {
-    document.getElementById('result').innerText = "Mohon masukkan nilai yang valid untuk suku pertama, rasio, dan jumlah suku.";
-    return;
-  }
-
-  if (r === 1) {
-    var result = a * n;
-  } else {
-    var result = (a * (1 - Math.pow(r, n))) / (1 - r);
-  }
-
-  document.getElementById('result').innerText = "Jumlah deret geometri: " + result;
-}
-
 function clearInputs() {
   document.getElementById('input').value = '';
   document.getElementById('input2').value = '';
