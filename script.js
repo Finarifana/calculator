d30f6d3fafcb48ca60a719754a7a5685dc80701e
fina <finarifana22@upi.edu>
2024-05-12 17:27:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function calculateArithmetic() {
+    var a = parseFloat(document.getElementById('input').value);
+    var d = parseFloat(document.getElementById('input2').value);
+    var n = parseInt(document.getElementById('n').value);
+  
+    if (isNaN(a) || isNaN(d) || isNaN(n)) {
+      document.getElementById('result').innerText = "Mohon masukkan nilai yang valid untuk suku pertama, beda, dan jumlah suku.";
+      return;
+    }
+  
+    var result = 0;
+    for (var i = 0; i < n; i++) {
+      result += a + i * d;
+    }
+  
+    document.getElementById('result').innerText = "Jumlah baris aritmatika: " + result;
+  }
+  
+  function calculateGeometric() {
+    var a = parseFloat(document.getElementById('input').value);
+    var r = parseFloat(document.getElementById('input2').value);
+    var n = parseInt(document.getElementById('n').value);
+  
+    if (isNaN(a) || isNaN(r) || isNaN(n)) {
+      document.getElementById('result').innerText = "Mohon masukkan nilai yang valid untuk suku pertama, rasio, dan jumlah suku.";
+      return;
+    }
+  
+    var result = 0;
+    for (var i = 0; i < n; i++) {
+      result += a * Math.pow(r, i);
+    }
+  
+    document.getElementById('result').innerText = "Jumlah baris geometri: " + result;
+  }
+  
